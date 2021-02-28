0138f51d5a8a973848456661d2fc151d91dd3f9b
Michael Stephens <mdstephens26@yahoo.com>
2021-02-28 16:07:04 -0500

    commit tree dc718b2df1b75596d52ea8f9a56fd8c80c809877

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    "homepage"
   end
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show  
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect "/articles"
+  end
+
 end
